bb3619734dbff086b3752f4e29ea4c7791e0f30b
Sabit Huraira <sabitzhabit@gmail.com>
2017-11-25 00:34:49 +0700

    failed test: if input 0 it should return 'zero'

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # ruby-tdd
 
 Sample testing Ruby code using RSpec
+
+* First make sure you install rspec with run 'gem install rspec' on terminal
+
+* run this command for core code: 'ruby lib/arabic2english.rb 899'
+* run this command for test using rspec: 'rspec spec/lib/arabic2english.rb'
---
+++ spec/lib/arabic2english_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require 'arabic2english'
+
+describe Arabic2english do
+    it "number to words" do
+        data = Arabic2english.new
+        expect(data.int_to_words(44)).to eql("forty four")
+        expect(data.int_to_words(0)).to eql("zero")
+        expect(data.int_to_words(150)).to eql("one hundred fifty")
+    end
+end
